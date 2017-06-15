b41e799bb609177e5a7f9eb995cc326b4863986e
scresswell-kainos <s.cresswell@kainos.com>
2017-06-15 17:12:10 +0100

    update to login.js

--- javascript/login.js
+++ javascript/login.js
@@ -3,14 +3,12 @@ module.exports = function (phantom, ready) {
     phantom.evaluate(function(){
       if (document.getElementById("loginform")!=null)
       {
-        phantom.echo('login required');
         // Enter username and password in the input fields
         document.getElementById("username-input").value="llclradmin@nonexistanttest.com";
         document.getElementById("password-input").value="password";
-        document.getElementById("btnSubmit").click()
-        }
-      });
-
+        document.getElementById("loginform").submit();
+      }
+    });
 
     phantom.open(phantom.url, function () {
       setTimeout(ready, 5000);
